bab554ad0db61eb7ffd541e35e55ecb81eda7e6b
Sahil Budhiraja <sbudhiraja@netflix.com>
2016-07-11 14:57:45 -0700

    file1

---
+++ journey_from_procedural/one.js
@@ -0,0 +1,8 @@
+/*
+1.1 multiply numbers sync and serially, with re-usability and testability
+1.2 multiply number async serially
+1.3 multiply number async in parallel
+
+2.1, 2.2, 2.3 above, but only for a small set of numbers from  a large list
+
+*
